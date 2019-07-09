49c22aa318f729d76896bf380a27ea9508e68990
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2019-07-09 15:20:47 -0400

    Added example for constants

---
+++ src/com/vid/kotlin/tidbits/Constants.kt
@@ -0,0 +1,12 @@
+package com.vid.kotlin.tidbits
+
+val CopyrightAuthor = "Jeff"
+// Contextual convention determines which style we follow
+object Constants {
+    val author = "Vid Harihara"
+}
+
+fun main() {
+    println(Constants.author)
+    println(CopyrightAuthor)
+}

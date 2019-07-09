8cc96d46c94d26a6acc4c1a279c0abfa36752d80
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2019-07-09 14:56:29 -0400

    Added examples for decontructing values

--- src/com/vid/kotlin/tidbits/Tuples.kt
+++ src/com/vid/kotlin/tidbits/Tuples.kt
@@ -1,5 +1,7 @@
 package com.vid.kotlin.tidbits
 
+import com.vid.kotlin.classes.CustomerKotlin
+
 
 fun capitalAndPopulation(country: String): Pair<String, Long> {
     return Pair("Madrid", 23000000)
@@ -20,5 +22,29 @@ fun main() {
     println(info.first)
     println(info.second)
     println(info.third)
+
+
+    val (capital, population) = capitalAndPopulation("Madrid")
+    println(capital)
+    println(population)
+
+    val (capital1, continent, population1) = countryInfo("Madrid")
+    println(capital1)
+    println(continent)
+    println(population1)
+
+    val (id, name, email) = CustomerKotlin(1, "Name", "email")
+    println(id)
+    println(name)
+    println(email)
+
+    // Syntactic Sugar
+    val listOfCapitalsAndCountries = listOf(Pair("Madrid", "Spain"), "Paris" to "France")
+
+    for((capital, country) in listOfCapitalsAndCountries) {
+        println("$capital - $country")
+    }
+
+
 
 }
